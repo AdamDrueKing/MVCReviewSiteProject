6ef1e9727bec720211bb675731c36e165ac39e0d
Adam King <adam81king@gmail.com>
2017-07-28 15:11:59 -0400

    initial commit

---
+++ MVCReviewSiteProject/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Display(Name="Location")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Reviews>Reviews { get; set; }
+    }
+}
---
+++ MVCReviewSiteProject/Models/Reviews.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [Display(Name = "Name")]
+        public string ReviewerName { get; set; }
+
+        [Display(Name = "Reviewer Location")]
+        public string ReviewerHome { get; set; }
+
+        [Display(Name = "Trip Date")]
+        public string TravelDate { get; set; }
+
+        [Display(Name = "Review Date")]
+        public DateTime ReviewDate { get; set; }
+
+        [Display(Name ="Review")]
+        public string Review { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
